ced52efde74123d4a4ddc7971bfef13d3342cd6b
Roberth <Roberth.evaldsson@hotmail.com>
2018-10-07 20:11:02 +0200

    Tasks are editable

--- assets/js/components/task.vue
+++ assets/js/components/task.vue
@@ -1,8 +1,26 @@
 <template>
-  <div class="list-group-item">
-    <h5> {{ title }}</h5>
-    <p> {{ description }}</p>
+  <div v-if="!editing">
+    <div class="list-group-item">
+      <h5 v-text="title"></h5>
+      <p v-text="description"></p>
+      <button class="btn btn-info" @click="editing = true">Edit</button>
+    </div>
   </div>
+  <div v-else>
+    <div class="list-group-item">
+      <form action="" @submit.prevent="update">
+        <div class="form-group">
+          <input class="form-control" type="text" name="title" id="title" placeholder="Title" v-model="title">
+        </div>
+        <div class="form-group">
+          <input class="form-control" type="text" name="description" id="description" placeholder="Description" v-model="description">
+        </div>
+        <button type="button" class="btn btn-danger" @click="editing = false">Cancel</button>
+        <button type="submit" class="btn btn-info" >Update</button>
+      </form>
+    </div>
+  </div>
+
 </template>
 
 <script>
@@ -11,9 +29,26 @@
         props: ['task'],
         data() {
             return {
+                id: this.task.id,
                 editing: false,
                 title: this.task.title,
                 description: this.task.description
+            }
+        },
+        methods: {
+            destroy(){
+
+            },
+            update(){
+              console.log('Update Called!');
+              axios.patch('/tasks/' + this.id, {
+                  title: this.title,
+                  description: this.description
+              }).catch(error => {
+                  console.log(error.response.data);
+              });
+
+              this.editing = false;
             }
         }
     }
--- src/Controller/TasksController.php
+++ src/Controller/TasksController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\Tasks;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TasksController extends AbstractController
@@ -18,6 +20,20 @@ class TasksController extends AbstractController
         return $this->render('tasks/index.html.twig', [
             'tasks' => $tasks
         ]);
+    }
+
+    public function update($id, Request $request)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        $repository = $this->getDoctrine()->getRepository(Tasks::class);
+        $task = $repository->find($id);
+
+        $task->setTitle($data['title']);
+        dd($data['title']);
+        $task->setDescription($data['description']);
+
+        return new response('Task have been updated', 200);
     }
 
     public function edit($id)
